9f7b915aa47b6671ca8389c5a2341a88d3eafb06
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-05-22 20:50:20 +0200

    Add base class for all responses models

--- response_schemas.py
+++ response_schemas.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra
 """Representation of all responses from api in object-oriented model. It is using also to validtion"""
 
-class Temparature(BaseModel):
+class PreconfiguredBaseModel(BaseModel):
+    """Base class with config for all responses models"""
+    class Config:
+        extra = Extra.forbid
+
+
+class Temparature(PreconfiguredBaseModel):
     ID: int | None
     TEMPERATURE: float
 
 
-class Humidity(BaseModel):
+class Humidity(PreconfiguredBaseModel):
     ID: int | None 
     HUMIDITY: float
 
 
-class Particles(BaseModel):
+class Particles(PreconfiguredBaseModel):
     ID: int | None
     PARTICLES: float
